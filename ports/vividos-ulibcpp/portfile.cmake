a75ba585413e80faff46bd6a8be9a8d3c77e8f31
Michael Fink <michael.fink@asamnet.de>
2023-02-27 08:09:04 +0100

    fixed build error by replacing $(SolutionDir) with $(MSBuildThisFileDirectory) instead of providing it in vcpkg_install_msbuild()

--- ports/vividos-ulibcpp/portfile.cmake
+++ ports/vividos-ulibcpp/portfile.cmake
@@ -5,7 +5,9 @@ vcpkg_from_github(
     REPO vividos/UlibCpp
     REF version-4.4.0
     SHA512 8ebf70c834b9e878c6534f30a3f7a10d24dafff23f2cd8e49e38b17027137d739725a1fe1e1905076213902e9e640082d5ff6ea4981dbda2bb45d781d2861f5e
-    PATCHES remove-nuget-restore-check.patch
+    PATCHES
+        remove-nuget-restore-check.patch
+        replace-solutiondir-in-props.patch
 )
 
 file(REMOVE ${SOURCE_PATH}/src/nupkg/nuget.exe)
@@ -16,6 +18,4 @@ vcpkg_install_msbuild(
     INCLUDES_SUBPATH src/include
     LICENSE_SUBPATH LICENSE.md
     USE_VCPKG_INTEGRATION
-    OPTIONS
-        "/p:SolutionDir=${SOURCE_PATH}/src/"
 )
