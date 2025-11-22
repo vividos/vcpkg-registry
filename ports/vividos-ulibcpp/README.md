ab8216942304233d95b887b8cf01e7bcba226f9c
Michael Fink <michael.fink@asamnet.de>
2025-11-23 00:00:40 +0100

    chore: updated existing port vividos-ulibcpp

--- ports/vividos-ulibcpp/README.md
+++ ports/vividos-ulibcpp/README.md
@@ -2,12 +2,7 @@
 
 This is a port of the [UlibCpp](https://github.com/vividos/UlibCpp) library.
 
-The port has some specialities:
-- The C++ `.vcxproj` project itself uses vcpkg in Manifest mode
-- Its `.props` file uses `$(SolutionDir)`, but we're building the `.vcxproj`
-  file alone, so a patch is necessary to remove that variable
-- It uses some NuGet packages for SourceLink; the check for that packages also
-  has to be disabled for the vcpkg build to succeed
+The C++ `.vcxproj` project itself uses vcpkg in Manifest mode.
 
 The advantage of using UlibCpp using vcpkg is that the user can choose which
 C runtime (static vs. dynamic) is used for the library, by setting
--- ports/vividos-ulibcpp/portfile.cmake
+++ ports/vividos-ulibcpp/portfile.cmake
@@ -3,12 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO vividos/UlibCpp
-    REF version-4.4.0
-    SHA512 8ebf70c834b9e878c6534f30a3f7a10d24dafff23f2cd8e49e38b17027137d739725a1fe1e1905076213902e9e640082d5ff6ea4981dbda2bb45d781d2861f5e
-    PATCHES
-        remove-nuget-restore-check.patch
-        replace-solutiondir-in-props.patch
-        runtime-library-property.patch
+    REF version-5.0.0
+    SHA512 3a41afdd80e9fe4f21b7605292d2b078acb58c59826bc0ec5693dc02dd34f7d92b646038460a55e4936e1f1e286e44cb488d968fe7f39062f277456a11cfe438
 )
 
 if(VCPKG_CRT_LINKAGE STREQUAL "static")
@@ -25,6 +21,7 @@ vcpkg_install_msbuild(
     INCLUDES_SUBPATH src/include
     LICENSE_SUBPATH LICENSE.md
     USE_VCPKG_INTEGRATION
+    OPTIONS "/p:DisableNuGetPackageCheck=true"
     OPTIONS_DEBUG "/p:RuntimeLibrary=MultiThreadedDebug${RuntimeLibraryExt}"
     OPTIONS_RELEASE "/p:RuntimeLibrary=MultiThreaded${RuntimeLibraryExt}"
 )
